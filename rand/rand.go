8b950cce01a7224f7505b68dda5729fbd7556f14
XiaoChao Dong (@damnever) <dxc.wolf@gmail.com>
2019-12-18 23:24:26 +0800

    Shortcut for rand.Rand.Shuffle

--- rand/rand.go
+++ rand/rand.go
@@ -2,9 +2,24 @@ package rand
 
 import (
 	crand "crypto/rand"
+	"math/rand"
+	"time"
 )
 
 // FIXME: Too slow
+
+// Shuffle is the shortcut for (new *rand.Rand).Shuffle,
+// because the global (package rand).Shuffle will generate same results.
+func Shuffle(n int, swap func(i, j int)) {
+	if n < 0 {
+		panic("invalid argument to Shuffle")
+	}
+	if n == 0 {
+		return
+	}
+	r := rand.New(rand.NewSource(time.Now().UnixNano())) // seed + rand.Int63n(1000000)?
+	r.Shuffle(n, swap)
+}
 
 func Bytes(p []byte) {
 	_, _ = crand.Read(p)
