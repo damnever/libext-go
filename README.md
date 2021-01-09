3e6eb4d48c5d7b54cb31ac7cb086ae921dba6b44
Xiaochao Dong (@damnever) <dxc.wolf@gmail.com>
2021-01-09 22:17:31 +0800

    docs: add more documentations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## libext-go
 
-Standard library extensions for Golang, maybe it is useless for you :-)
+[![Go Reference](https://pkg.go.dev/badge/github.com/damnever/libext-go.svg)](https://pkg.go.dev/github.com/damnever/libext-go)
+
+Standard library extensions for Golang, maybe it is useful to you too :-)
--- io/rewindable.go
+++ io/rewindable.go
@@ -5,6 +5,8 @@ import (
 	"io"
 )
 
+// RewindableReader is a reader which can be rewind and reads from begin again.
+// It is not thread-safe.
 type RewindableReader struct {
 	r       io.Reader
 	buf     *bytes.Buffer
@@ -12,33 +14,39 @@ type RewindableReader struct {
 	rewind  bool
 }
 
+// NewRewindableReader creates a new RewindableReader.
 func NewRewindableReader(r io.Reader) *RewindableReader {
-	buf := &bytes.Buffer{}
-	return &RewindableReader{
-		r:       io.TeeReader(r, buf),
-		buf:     buf,
-		bufread: 0,
-		rewind:  false,
+	rr := &RewindableReader{
+		buf: &bytes.Buffer{},
 	}
+	rr.Reset(r)
+	return rr
 }
 
+// Reset resets the internal state by given reader.
 func (rr *RewindableReader) Reset(r io.Reader) {
 	rr.rewind = false
 	rr.buf.Reset()
 	rr.bufread = 0
-	rr.r = r
+	rr.r = io.TeeReader(r, rr.buf)
 }
 
+// Rewind resets the read offset to the begin.
 func (rr *RewindableReader) Rewind() {
 	rr.rewind = true
 	rr.bufread = 0
 }
 
+// Read implements io.Reader. It always returns len(p) and a nil error, otherwise the error is not nil.
 func (rr *RewindableReader) Read(p []byte) (int, error) {
 	if rr.rewind && rr.buf.Len() > rr.bufread {
 		n := copy(p, rr.buf.Bytes()[rr.bufread:])
 		rr.bufread += n
-		return n, nil
+		if n == len(p) {
+			return n, nil
+		}
+		// Short.
+		p = p[n:]
 	}
 	return rr.r.Read(p)
 }
