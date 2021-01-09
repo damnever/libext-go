00d9eb8eb474ad55bba38441044bd5f4c2557a74
Xiaochao Dong (@damnever) <dxc.wolf@gmail.com>
2021-01-09 22:17:48 +0800

    refactor!: change interfaces and make slice pool more configurable

--- bytes/buffer_pool.go
+++ bytes/buffer_pool.go
@@ -5,10 +5,12 @@ import (
 	"sync"
 )
 
+// BufferPool manages a pool of bytes.Buffer, the underlying pool is sync.Pool.
 type BufferPool struct {
 	pool sync.Pool
 }
 
+// NewBufferPool returns a new BufferPool.
 func NewBufferPool() *BufferPool {
 	return &BufferPool{
 		pool: sync.Pool{
@@ -19,10 +21,12 @@ func NewBufferPool() *BufferPool {
 	}
 }
 
+// Get returns a bytes.Buffer from pool.
 func (p *BufferPool) Get() *bytes.Buffer {
 	return p.pool.Get().(*bytes.Buffer)
 }
 
+// Put puts the bytes.Buffer back into pool.
 func (p *BufferPool) Put(buf *bytes.Buffer) {
 	buf.Reset()
 	p.pool.Put(buf)
